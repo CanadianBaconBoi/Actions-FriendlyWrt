24a633429d197d1f836eefed108e0996d5ff4c53
CanadianBaconBoi <bc.bacon.bits@gmail.com>
2024-10-22 08:27:54 +0200

    Update custome_config.sh

--- scripts/custome_config.sh
+++ scripts/custome_config.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sed -i -e '/CONFIG_MAKE_TOOLCHAIN=y/d' configs/rockchip/01-nanopi
 sed -i -e 's/CONFIG_IB=y/# CONFIG_IB is not set/g' configs/rockchip/01-nanopi
-sed -i -e 's/CONFIG_SDK=y/# CONFIG_SDK is not set/g' configs/rockchip/01-nanopi
+echo 'CONFIG_HEADERS_INSTALL=y' >> configs/rockchip/01-nanopi
+echo 'CONFIG_PACKGE_bpf-headers=y' >> configs/rockchip/01-nanopi
